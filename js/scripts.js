1f35d20e445fc42759d3ee4520640c5c23135d4b
tkm404 <tmcdowellddhs@yahoo.com>
2023-04-07 08:23:03 -0700

    add topping property to Pizza object.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-function Pizza(size) {
+function Pizza(size, topping) {
   this.size = size
+  this.toppings = [topping]
 }
 
-const smallPizza = new Pizza("small")
+const smallPizza = new Pizza("small", ["pepperoni", "olives"])
 console.log(smallPizza)
